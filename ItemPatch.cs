3499c42934b0a61d313bb7a2e2cefa265ace3ff3
N-cognito <tre.t.business@gmail.com>
2025-04-17 00:36:34 -0400

    Initial commit

---
+++ ItemPatch.cs
@@ -0,0 +1,77 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using static PlayerHealth;
+using System.Reflection.Emit;
+using System.ComponentModel;
+
+namespace ItemsBounceBack
+{
+    [HarmonyPatch(typeof(StatsManager))]
+    internal static class ItemBouncePatch_StatsManager
+    {
+        [HarmonyPostfix, HarmonyPatch(nameof(StatsManager.Start))]
+        public static void Start_Postfix(StatsManager __instance)
+        {
+            ItemsBounceBack.PopulateDictionary();
+        }
+    }
+
+    [HarmonyPatch(typeof(HurtCollider))]
+    internal static class ItemBouncePatch_HurtCollider
+    {
+
+        [HarmonyTranspiler, HarmonyPatch(nameof(HurtCollider.ColliderCheck), MethodType.Enumerator)]
+        static IEnumerable<CodeInstruction> ColliderCheck_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codeMatcher = new CodeMatcher(instructions /*, ILGenerator generator*/);
+
+            //
+            // Expected Behavior: Add check to ColliderCheck() Line 227
+            // if (!componentInParent4.destroyDisable)
+            // to
+            // if (!componentInParent4.destroyDisable || !ItemsBounceBack.ShouldItemBounce(componentInParent4.gameObject))
+            //
+
+            // Expect IL_03CE
+            codeMatcher.MatchForward(true, (CodeMatch[])(object)new CodeMatch[7]
+            {
+                new CodeMatch((OpCode?)OpCodes.Brtrue),
+                new CodeMatch((OpCode?)OpCodes.Ldloc_1),
+                new CodeMatch((OpCode?)OpCodes.Ldfld),
+                new CodeMatch((OpCode?)OpCodes.Brfalse),
+                new CodeMatch((OpCode?)OpCodes.Ldloc_S),
+                new CodeMatch((OpCode?)OpCodes.Ldfld),
+                new CodeMatch((OpCode?)OpCodes.Brtrue)
+            })
+            .ThrowIfInvalid("ColliderCheck(): Couldn't find matching code");
+
+            // IL_03CE, label IL_03F5
+            var exitOperand = codeMatcher.Operand;
+
+            codeMatcher.Advance(-1);
+            codeMatcher.Advance(-1);
+            // IL_03C7, label V_17
+            var variableOperand = codeMatcher.Operand;
+
+            //Go back to above codematch+1
+            codeMatcher.Advance(1);
+            codeMatcher.Advance(1);
+            codeMatcher.Advance(1);
+
+            codeMatcher.Insert((CodeInstruction[])(object)new CodeInstruction[4]
+            {
+                // store V_17 (componentInParent4 or PhyGrabObjectImpactDetector) to stack
+			    new CodeInstruction(OpCodes.Ldloc_S, variableOperand),
+                // get gameObject
+                new CodeInstruction(OpCodes.Call, (object)AccessTools.Method(typeof(UnityEngine.Component), "get_gameObject")),
+                // call ItemsBounceBack.ShouldItemBounce, which checks for ItemAttributes component, then check ItemAttributes.itemAssetName against a list,
+                new CodeInstruction(OpCodes.Call, (object)AccessTools.Method(typeof(ItemsBounceBack), nameof(ItemsBounceBack.ShouldItemBounce))),
+                // move to IL_03F5 if above statement is true
+			    new CodeInstruction(OpCodes.Brtrue, exitOperand)
+            });
+            return codeMatcher.InstructionEnumeration();
+        }
+    }
+}
---
+++ ItemsBounceBack.cs
@@ -0,0 +1,89 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using REPOLib.Extensions;
+using Steamworks.Ugc;
+
+namespace ItemsBounceBack
+{
+    [BepInPlugin("SeroRonin.ItemsBounceBack", "ItemsBounceBack", "1.1.0")]
+    [BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+    public class ItemsBounceBack : BaseUnityPlugin
+    {
+        public static ItemsBounceBack Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger => Instance._logger;
+        private ManualLogSource _logger => base.Logger;
+        internal Harmony? Harmony { get; set; }
+
+        public Dictionary<string, bool> shouldItemBounce = new Dictionary<string, bool>();
+
+        private void Awake()
+        {
+            Instance = this;
+
+            // Prevent the plugin from being deleted
+            this.gameObject.transform.parent = null;
+            this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            Patch();
+
+            Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");   
+        }
+
+        internal void Patch()
+        {
+            Harmony ??= new Harmony(Info.Metadata.GUID);
+            Harmony.PatchAll();
+        }
+
+        internal void Unpatch()
+        {
+            Harmony?.UnpatchSelf();
+        }
+
+        public static bool ShouldItemBounce( GameObject gameObject )
+        {
+            var itemComp = gameObject.GetComponent<ItemAttributes>();
+            var itemEquip = gameObject.GetComponent<ItemEquippable>();
+            var shouldBounce = false;
+            if ( itemComp && itemEquip && !SemiFunc.RunIsArena())
+            {
+                Instance.shouldItemBounce.TryGetValue(itemComp.itemAssetName, out shouldBounce);
+            }
+
+            return shouldBounce;
+        }
+
+        public static bool TryAddBounceEntry( string assetName, bool shouldBounce = true, bool overrideEntry = true )
+        {
+            if ( !Instance.shouldItemBounce.ContainsKey(assetName) )
+            {
+                Instance.shouldItemBounce.Add(assetName, shouldBounce);
+                return true;
+            }
+            else
+            {
+                if (overrideEntry)
+                {
+                    Instance.shouldItemBounce[assetName] = shouldBounce;
+                }
+            }
+
+            return false;
+        }
+
+        public static void PopulateDictionary()
+        {
+            var itemList = StatsManager.instance.GetItems();
+            foreach ( var item in itemList )
+            {
+                // These item types should not bounce by default, individual entries can be overriden by developers in their own code
+                var shouldBounce = ( !(item.itemType == SemiFunc.itemType.item_upgrade) && !(item.itemType == SemiFunc.itemType.cart) && !(item.itemType == SemiFunc.itemType.pocket_cart) );
+
+                TryAddBounceEntry(item.itemAssetName, shouldBounce, false);
+            }
+        }
+    }
+}
